be158c9a06a03a513831c9cedb3aaf4f6e9bed41
William <lipw0755@gmail.com>
2024-07-31 19:37:21 +0800

    fix layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import * as React from "react";
 import Banner from "./components/banner";
 import Link from "next/link";
@@ -6,7 +7,7 @@ import { ro } from "date-fns/locale";
 import { cn } from "@/lib/utils";
 import { usePathname, useRouter } from "next/navigation";
 import MainMenu from "./components/main-menu";
-import { useMediaQuery } from "@uidotdev/usehooks";
+import { useMediaQuery } from "@/hooks/use-media-query";
 import { useState } from "react";
 import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 import { MenuIcon, XIcon } from "lucide-react";
@@ -24,7 +25,7 @@ const DashBoardLayout = ({ children }: Props) => {
       <div className="md:grid md:grid-cols-[250px_1fr] h-screen">
         <MainMenu className="hidden md:flex" />
         {!isDesktop && (
-          <div className="flex p-4 justify-between sticky top-0 left-0">
+          <div className="p-4 flex justify-between md:hidden sticky top-0 left-0 bg-background border-b border-border">
             <Banner />
             <Drawer
               direction="right"
@@ -39,8 +40,8 @@ const DashBoardLayout = ({ children }: Props) => {
               </DrawerTrigger>
               <DrawerContent>
                 <div className="flex justify-between p-4">
-                  <Banner/>
-                  <XIcon size={25} onClick={()=>setMobileMenuOpen(false)}/>
+                  <Banner />
+                  <XIcon size={25} onClick={() => setMobileMenuOpen(false)} />
                 </div>
                 <MainMenu />
               </DrawerContent>
